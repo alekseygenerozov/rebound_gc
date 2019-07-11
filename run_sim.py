ce31cce1ba0cd430a0d3b4bc8968ac36cebeeaa9
alekseygenerozov <aleksey.generozov@gmail.com>
2019-07-10 19:17:28 -0600

    better defaults and more control over reboundx parameters.

--- run_sim.py
+++ run_sim.py
@@ -21,9 +21,12 @@ import cgs_const as cgs
 def get_last_index(loc="."):
 	trials=bc.bash_command("echo trial_*").decode('utf-8')
 	trials=shlex.split(trials)
-	trials=np.array([tt.replace('trial_','') for tt in trials]).astype(int)
-	end=np.sort(trials)[-1]
-	return end+1
+	if (trials[0]=='trial_*'):
+		return 1
+	else:
+		trials=np.array([tt.replace('trial_','') for tt in trials]).astype(int)
+		end=np.sort(trials)[-1]
+		return end+1
 
 
 def rotate_vec(angle,axis,vec):    
@@ -111,9 +114,10 @@ def main():
 	##Default stellar parameters 
 	config=configparser.SafeConfigParser(defaults={'name': 'archive', 'N':'100', 'e':'0.7',
 		'gravity':'compensated', 'integrator':'ias15', 'dt':'0', \
-		'a_min':'1.', 'a_max':'2.', 'ang1_mean':'0', 'ang2_mean':'0', 'ang3_mean':'0', 'ang1':'3.',\
-		 'ang2':'3.', 'ang3':'3.', 'keep_bins':'False', 'coll':'line', 'pRun':'0.1', 'pOut':'0.1', 
-		'p':'2', 'frac':'nan', 'outDir':'./'}, dict_type=OrderedDict)
+		'a_min':'0.05', 'a_max':'0.5', 'ang1_mean':'0', 'ang2_mean':'0', 'ang3_mean':'0', 'ang1':'2.',\
+		 'ang2':'2.', 'ang3':'2.', 'keep_bins':'False', 'coll':'line', 'pRun':'0.1', 'pOut':'0.1', 
+		'p':'1', 'frac':'2.5e-3', 'outDir':'./', 'gr':'True', 'rinf':'4.0', 'alpha':'1.5',
+		'rt':'3.57e-5'}, dict_type=OrderedDict)
 	# config.optionxform=str
 	config.read(config_file)
 
@@ -127,6 +131,10 @@ def main():
 	keep_bins=config.getboolean('params', 'keep_bins')
 	# rt=config.getfloat('params', 'rt')
 	coll=config.get('params', 'coll')
+	gr=config.getboolean('params', 'gr')
+	rinf=config.getfloat('params', 'rinf')
+	alpha=config.getfloat('params', 'alpha')
+	print(gr)
 
 	#print pRun, pOut, rt, coll
 	sections=config.sections()
@@ -160,9 +168,8 @@ def main():
 		e=config.getfloat(ss, 'e')
 		##rescale stellar masses so they are a fixed fraction of the central mass.
 		frac=config.getfloat(ss, 'frac')
-		if not np.isnan(frac):
-			mbar=sim.particles[0].m*frac/num[ss]
-			print(mbar)
+		mbar=sim.particles[0].m*frac/num[ss]
+		print(mbar)
 		a_min=config.getfloat(ss, 'a_min')
 		a_max=config.getfloat(ss, 'a_max')
 		p=config.getfloat(ss, 'p')
@@ -172,17 +179,21 @@ def main():
 		ang2=config.getfloat(ss, 'ang2')
 		ang3_mean=config.getfloat(ss, 'ang3_mean')
 		ang3=config.getfloat(ss, 'ang3')
+		##We can generalize this to be a function?
+		rt=config.getfloat(ss, 'rt')
 
 		N0=len(sim.particles)
 		for l in range(0,N): # Adds stars
 			##Use AM's code to generate disk with aligned eccentricity vectors, but a small scatter in i and both omegas...
 			inc, Omega, omega=gen_disk(ang1*np.pi/180., ang1_mean*np.pi/180., ang2*np.pi/180., ang2_mean*np.pi/180., ang3*np.pi/180., ang3_mean*np.pi/180.0)
 			a0=density(a_min, a_max, p)
-			m=density(1., 60., 1.7)*(mbar/6.0)
+			##Better way to include the mass spectrum...name of function define MF as a parameter.
+			# m=density(1., 60., 1.7)*(mbar/6.0)
+			m=mbar
 			M = rand.uniform(0., 2.*np.pi)
-			print(m, (sim.particles[0].m/m)**(1./3.)*0.1*cgs.au/cgs.pc)
+			# print(m, (sim.particles[0].m/m)**(1./3.)*0.1*cgs.au/cgs.pc)
 			sim.add(m = m, a = a0, e = e, inc=inc, Omega = Omega, omega = omega, M = M, primary=sim.particles[0],\
-				r=(sim.particles[0].m/m)**(1./3.)*0.1*cgs.au/cgs.pc)
+				r=rt)
 		##Indices of each component
 		nparts[ss]=(N0,N0+N-1)
 	
@@ -236,22 +247,24 @@ def main():
 	sim.collision_resolve=get_tde
 	
 	##Stellar potential
-	rinf=4.0
-	alpha=1.5
 	rebx = reboundx.Extras(sim)
-	menc=rebx.add("menc")
-	menc.params["rinf"]=rinf
-	menc.params["alpha"]=alpha
+	if rinf>0:
+		menc=rebx.add("menc")
+		menc.params["rinf"]=rinf
+		menc.params["alpha"]=alpha
 	##GR effects
-	gr=rebx.add("gr")
-	gr.params["c"]=4571304.57795483
+	if gr:
+		gr=rebx.add("gr")
+		##Speed of light in simulation units.
+		gr.params["c"]=4571304.57795483
 
 	##Set up simulation archive for output
+	# sa = rebound.SimulationArchive(loc+name, rebxfilename='rebx.bin')
 	sim.automateSimulationArchive(loc+name,interval=pOut*pRun,deletefile=True)
 	sim.heartbeat=heartbeat
 	sim.move_to_com()
 	sim.simulationarchive_snapshot(loc+name)
-
+	bc.bash_command('cp {0} {1}'.format(config_file, loc))
 
 	en=sim.calculate_energy()
 	print(sim.N, rebound.__version__)
