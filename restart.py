3f6bc4c75752411ab2835a8b89a28cea81776c0f
alekseygenerozov <aleksey.generozov@gmail.com>
2019-08-10 18:55:24 -0600

    Rewriting restart code.

--- restart.py
+++ restart.py
@@ -1,29 +1,86 @@
 import rebound
 import sys
 import numpy as np
+from run_sim import get_tde, get_tde_no_delR, heartbeat
 
-def heartbeat(sim):
-	print(sim.contents.dt, sim.contents.t)
-# sim is a pointer to the simulation object,
-# thus use contents to access object data.
-# See ctypes documentation for details.
-	# print(sim.contents.dt)
 
-def get_tde(sim, reb_coll):
-	orbits = sim[0].calculate_orbits(primary=sim[0].particles[0])
-	p1,p2 = reb_coll.p1, reb_coll.p2
-	idx, idx0 = max(p1, p2), min(p1, p2)
-	if idx0==0:
-		##idx decremented by 1 because there is no orbit 0
-		print sim[0].t, orbits[idx-1].e, idx, 'TDE!'
+def main():
+	parser=argparse.ArgumentParser(
+		description='Set up a rebound run')
+	parser.add_argument('--config', nargs=1, default='config',
+		help='File containing simulation parameters')
+	parser.add_argument('--index', '-i', type=int, default=1,
+		help='Index of output file')
+	# parser.add_argument('--keep_bins', action='store_true',
+	# 	help="Don't delete bins from simulation")
 
-	return 0
 
-tmax = 500.
-sim = rebound.Simulation.from_archive(sys.argv[1])
-sim.automateSimulationArchive(sys.argv[1],interval=np.pi*0.2,deletefile=False)
-if sim.t>=tmax*2*np.pi:
-	sys.exit(0)
-sim.simulationarchive_next=sim.t+0.2*np.pi	
-sim.collision_resolve=get_tde
-sim.integrate(tmax*2*np.pi)
+	##Parsing command line arguments.
+	args=parser.parse_args()
+	config_file=args.config
+	loc="trial_{0}/".format(args.index)
+
+	config=configparser.SafeConfigParser(defaults={'name': 'archive', 'N':'100', 'e':'0.7',
+		'gravity':'compensated', 'integrator':'ias15', 'dt':'0', \
+		'a_min':'0.05', 'a_max':'0.5', 'ang1_mean':'0', 'ang2_mean':'0', 'ang3_mean':'0', 'ang1':'2.',\
+		 'ang2':'2.', 'ang3':'2.', 'keep_bins':'False', 'coll':'line', 'pRun':'0.1', 'pOut':'0.1', 
+		'p':'1', 'frac':'2.5e-3', 'outDir':'./', 'gr':'True', 'rinf':'4.0', 'alpha':'1.5',
+		'rt':'1e-4', 'mf':"mfixed", 'merge':'False', 'menc_comp':'False', 'Mbh':'4e6',
+		'c':'4571304.57795483', 'delR':'True'}, dict_type=OrderedDict)
+
+	##Name of our put file 
+	name=config.get('params', 'name')
+	name=name+".bin"
+	name=config.get('params', 'outDir')+'/'+name
+	##Length of simulation and interval between snapshots
+	pRun=config.getfloat('params', 'pRun')
+	pOut=config.getfloat('params', 'pOut')
+	coll=config.get('params', 'coll')
+	gr=config.getboolean('params', 'gr')
+	rinf=config.getfloat('params', 'rinf')
+	alpha=config.getfloat('params', 'alpha')
+	menc_comp=config.getboolean('params', 'menc_comp')
+	Mbh=config.getfloat('params', 'Mbh')
+	a_min=config.getfloat(ss, 'a_min')
+
+
+	sim=rebound.from_archive(loc+'/archive.bin')
+	sim.heartbeat=heartbeat
+	sim.collision=coll
+	sim.collision_resolve=get_tde
+	delR=config.getboolean('params', 'delR')
+	merge=config.getboolean('params', 'merge')
+	if merge:
+		sim.collision_resolve='merge'
+	if not delR:
+		sim.collision_resolve=get_tde_no_delR
+
+	##Stellar potential
+	rebx = reboundx.Extras(sim)
+	if rinf>0:
+		if menc_comp:
+			print("test!")
+			menc=rebx.add("menc_comp")
+		else:
+			menc=rebx.add("menc")
+		menc.params["rinf"]=rinf
+		menc.params["alpha"]=alpha
+	##GR effects
+	if gr:
+		gr=rebx.add("gr")
+		##Speed of light in simulation units.
+		gr.params["c"]=config.getfloat('params', 'c')
+	sim.automateSimulationArchive(loc+name,interval=pOut*pRun,deletefile=False)
+	p_in=2.0*np.pi*(a_min**3.0/Mbh)**0.5
+	while(t<pRun):
+		if t>=orb_idx*delta_t:
+			orbits=sim.calculate_orbits(primary=sim.particles[0])
+			np.savetxt(loc+name.replace('.bin', '_out_{0}.dat'.format(orb_idx)), [[oo.a, oo.e, oo.inc, oo.Omega, oo.omega, oo.f] for oo in orbits])
+			orb_idx+=1
+		sim.move_to_com()
+		sim.integrate(sim.t+p_in)
+		##Should increment line below by pin not deltat
+		t+=p_in
+
+
+
